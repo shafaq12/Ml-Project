142786888007e016f64ef75ac2025732e040523c
Shafaq <shafaqayyub9@gmail.com>
2023-08-20 23:52:26 +0500

    Data ingestion

--- src/exception.py
+++ src/exception.py
@@ -1,17 +1,20 @@
 # custom exceptiom 
-import sys 
+import sys
 from src.logger import logging
-def error_message_detail(error,error_detail:sys):
-  _,_,exc_tb=error_detail.exc_info()
-  file_name = exc_tb.tb_frame.f_code.co_filname
-  error_message ="Error has occured in python script name [{0}]line number[{1}]error message[{2}]".format(
-    file_name,exc_tb.tb_lineno,str(error)
-  )
-  return error_message
-  #creating custom class for exception handling 
-  class CustomException(Exception):
-    def __init__(self,error_message,error_detail:sys):
-      super().__init__(error_message)
-      self.error_message = error_message_detail(error,error_detail=error_detail)
+
+def error_message_detail(error, error_detail: sys):
+    _, _, exc_tb = error_detail.exc_info()
+    file_name = exc_tb.tb_frame.f_code.co_filename
+    error_message = "Error has occurred in python script name [{0}] line number [{1}] error message [{2}]".format(
+        file_name, exc_tb.tb_lineno, str(error)
+    )
+    return error_message
+
+# Creating custom class for exception handling
+class CustomException(Exception):
+    def __init__(self, error_message, error_detail: sys):
+        super().__init__(error_message)
+        self.error_message = error_message_detail(error, error_detail=error_detail)
+        
     def __str__(self):
-      return self.error_message
+        return self.error_message
